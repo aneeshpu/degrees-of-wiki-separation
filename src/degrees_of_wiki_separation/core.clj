0fdebb8df4fa8aace0546838ab8c0f6477a82e23
Aneesh P U <aneeshp@thoughtworks.com>
2013-11-16 19:45:02 +0530

    A working version with lots of repeating searches

--- src/degrees_of_wiki_separation/core.clj
+++ src/degrees_of_wiki_separation/core.clj
@@ -2,16 +2,61 @@
   (:require [clj-http.client :as client]
             [net.cgrand.enlive-html :as html]))
 
+(defn is-image
+  [url]
+  (some #(.endsWith url %) ["jpeg" "gif"]))
+
 (defn get-page
   [url]
-  (html/html-resource (java.net.URL. url)))
+  (if
+    (and
+      (not= nil url)
+      (not (is-image url)))
+    (try
+      (html/html-resource
+        (java.net.URL.
+          (if
+            (.startsWith url "http:")
+            url
+            (.concat "http:" url))))
+      (catch java.io.FileNotFoundException fofe
+        (println (.getMessage fofe))))))
 
-(defn get-links
-  [url]
-  (map #(get % :href ) (map #(get % :attrs ) (html/select (get-page url) [:a ]))))
+  (defn get-links
+    [url]
+    (map #(get % :href )
+      (map #(get % :attrs )
+        (html/select
+          (get-page url)
+          [:a ]))))
 
 
-(defn is-link-present
-  [url target]
-  (reduce #(or %1 %2) (map #(if (= % target)
-                              true false) (get-links url))))
+  (defn is-link-present
+    [urls target]
+    (reduce #(or %1 %2) (map #(= % target) urls)))
+
+  (def not-nil?
+    (complement nil?))
+
+  (defn is-valid
+    [url]
+    (and (not-nil? url)
+      (not (.startsWith url "#"))
+      (or (.startsWith url "http://")
+        (.startsWith url "//"))))
+
+
+  (defn find-link
+    [url target depth]
+    (println "searching for " target " on " url)
+    (let [links (get-links url)]
+      (if (and (> 0 (count links)) (is-link-present links target))
+        (println "found " target " at " url)
+        (if (<= depth 0)
+          (println "could not find " target "on " url)
+          ((println "searching deeper. Is links == nil?" (nil? links))
+            (doseq [link links]
+              (if (is-valid link)
+                (find-link link target (dec depth)))
+              ;#(find-link % target (dec depth))
+              ))))))
--- test/degrees_of_wiki_separation/core_test.clj
+++ test/degrees_of_wiki_separation/core_test.clj
@@ -1,17 +1,16 @@
 (ns degrees-of-wiki-separation.core-test
-  (:require [clojure.test :refer :all]
-            [degrees-of-wiki-separation.core :refer :all]))
+  (:require [clojure.test :refer :all ]
+            [degrees-of-wiki-separation.core :refer :all ]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 0))))
 
 (comment
-(deftest returns-link-if-found-test
-  (testing
-    (is (= (find-link "http://en.wikipedia.org/wiki/Albert_Einstein", "http://en.wikipedia.org/wiki/Quantum_mechanics", 2) true))))
+  (deftest returns-link-if-found-test
+    (testing
+      (is (= (find-link "http://en.wikipedia.org/wiki/Albert_Einstein", "http://en.wikipedia.org/wiki/Quantum_mechanics", 2) true))))
   )
 
-(deftest boot-strap-test
-  (get-links "http://en.wikipedia.org/wiki/Linus_Torvalds"))
-
+(deftest find-link-test
+  (find-link "http://en.wikipedia.org/wiki/Linus_Torvalds" "/wiki/Gustav_I_of_Sweden" 3))
